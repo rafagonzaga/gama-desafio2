220ab881128f8ff6db0a3f6f6e61bb80660d7282
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-25 00:34:48 -0300

    Nova alteração da baseURL no api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: "http://localhost:8888/produtos"
-    baseURL: "https://my-json-server.typicode.com/rafagonzaga/json-server"
+    baseURL: "https://my-json-server.typicode.com/rafagonzaga/json-server/produtos"
 })
 
 export default api;
