b0ed94cfbbfbc2a8cf1d9a8a21dd9cec53b69f6e
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-25 00:11:27 -0300

    Tentativa de subida do server-json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8888/produtos"
+    // baseURL: "http://localhost:8888/produtos"
+    baseURL: "https://my-json-server.typicode.com/rafagonzaga/json-server/produtos"
 })
 
 export default api;
